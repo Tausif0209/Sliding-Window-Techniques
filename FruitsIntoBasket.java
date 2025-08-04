4c9a4dba637d690352c8e5da3f6ccbb514fc0336
Tausif0209 <tk027592@gmail.com>
2025-08-04 15:21:33 +0530

    Leetcode 904: Fruits Into Baskets

---
+++ FruitsIntoBasket.java
@@ -0,0 +1,19 @@
+public class FruitsIntoBasket {
+  public int totalFruit(int[] fruits) {
+        if(fruits.length==1) return 1;
+        int max=0;
+        HashMap<Integer,Integer> frequency=new HashMap<>();
+        int i=0,j=0;
+        while(j<fruits.length){
+            frequency.put(fruits[j],frequency.getOrDefault(fruits[j],0)+1);
+            while(frequency.size()>2){
+                int val=frequency.get(fruits[i]);
+                if(val==1) frequency.remove(fruits[i]);
+                else frequency.put(fruits[i],val-1);
+                i++;
+            } 
+            max=Math.max(max,j-i+1);
+            j++;
+        } return max;
+    }
+}
